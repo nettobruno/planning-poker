5a103864a9ad3d6f7158a5954c611abda166f7ab
Bruno Netto <bruno.rosa@grupoboticario.com.br>
2024-05-22 20:31:00 -0300

    get items from firebase db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import { listenToCollection } from '../services/firebase/firebaseFirestore'
+
 export default function Home() {
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getItemsFirebase = listenToCollection(
+      'users',
+      (dataFromFirestore) => {
+        setData(dataFromFirestore);
+        setLoading(false);
+      },
+      (error) => {
+        console.error('Erro ao ouvir dados do Firestore:', error);
+      }
+    );
+
+    return () => {
+      getItemsFirebase();
+    };
+  }, []);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
   return (
     <main>
       <h1>Hello World</h1>
+
+      {data.map((item: any, index: any) => (
+        <ul key={index}>
+          <li>{item.name}</li>
+          <li>{item.lastname}</li>
+          <li>{item.age}</li>
+        </ul>    
+      ))}
     </main>
   );
 }
